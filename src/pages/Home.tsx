3e4476b8eb6712d2b17740d1d068cc24fbc885c8
Merahimi <mohammaderahimi80@gmail.com>
2023-12-20 11:06:34 +0330

    Clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,13 @@ import { AboutUsWhitoutBanner } from "./AboutUs";
 import { ProductCarousel } from "../components/ProductCarousel";
 import Carousel from "react-bootstrap/Carousel";
 
-
 export function Home() {
   return (
     <Fragment>
       <Container fluid className="Slider p-0">
         <Carousel fade>
           <Carousel.Item>
-            <img
-              src="../../Imgs/Sliders/HomePageSlider01.jpeg"
-              alt="pic"
-            />
+            <img src="../../Imgs/Sliders/HomePageSlider01.jpeg" alt="pic" />
             <Carousel.Caption>
               <h1>
                 <strong>Tech</strong> Shop
@@ -23,10 +19,7 @@ export function Home() {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              src="../../Imgs/Sliders/HomePageSlider02.jpeg"
-              alt="pic"
-            />
+            <img src="../../Imgs/Sliders/HomePageSlider02.jpeg" alt="pic" />
             <Carousel.Caption>
               <h1>Tech Shop</h1>
               <h5>have great time with tech shop</h5>
