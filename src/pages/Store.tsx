6ac2dc5e78b68d7755ea7891028c617ac4cbd449
MErahimi <mohammaderahimi80@gmail.com>
2023-12-29 16:36:17 +0330

    add paging

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import { Banner } from "../components/Banner";
-import { ProductCardsList } from "../components/ProductCardsList";
+import { ProductPaging } from "../components/ProductCardsList";
 
 export function Store() {
   return (
     <Fragment>
       <Banner Url="../../Imgs/Sliders/StoreSlider.jpg" Message="Store" />
-      <ProductCardsList />
+      <ProductPaging />
     </Fragment>
   );
 }
